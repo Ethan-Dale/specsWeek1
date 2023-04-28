88040c4285873d7051e1919fe6f01b455ad07f6f
Ethan Dale <ethan.dale.147@gmail.com>
2023-04-28 16:02:44 -0600

    initial commit

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,55 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Square from "./components/Square";
+import { useState } from "react";
+
+function App() {
+  function calculateWinner(arr) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (arr[a] && arr[a] === arr[b] && arr[a] === arr[c]) {
+        return `${arr[a]} won!`;
+      }
+    }
+    return "Who will win?";
+  }
+  const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState(true);
+  const handleClick = () => {
+    setSquares(["", "", "", "", "", "", "", "", ""]);
+    setPlayer(true);
+  };
+
+  return (
+    <div className="App">
+      <div className="container">
+        {squares.map((val, index) => {
+          return (
+            <Square
+              setSquares={setSquares}
+              index={index}
+              squareValue={val}
+              squares={squares}
+              player={player}
+              setPlayer={setPlayer}
+            />
+          );
+        })}
+        <button onClick={handleClick}>Reset</button>
+        <span>{calculateWinner(squares)}</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
